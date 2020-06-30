41e3bf0cb8d62f407bebe1b0999e8bf09dfc3954
Evgrny <aevgenij45@gmail.com>
2020-06-30 16:15:34 +0300

    Signed-off-by: Evgrny <aevgenij45@gmail.com>

---
+++ frst.bat
@@ -0,0 +1,6 @@
+rem echo "# first" >> README.md
+rem git init
+rem git add README.md
+rem git commit -m "first commit"
+rem git remote add origin https://github.com/AEvgenij/first.git
+rem git push -u origin master
